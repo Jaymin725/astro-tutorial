c1cdf79201b6176a54641ec71a0f680c1c4b1fac
Jaymin725 <jayminbhavsar07@gmail.com>
2026-07-02 12:18:22 +0530

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-313725.netlify.app"
+  site: "https://astro-tutorial-313725.netlify.app",
+  integrations: [preact()]
 });
